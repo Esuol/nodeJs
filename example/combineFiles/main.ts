c0ac0d56f1c2779136a7b25cd9eedf8d7e8c7f00
berlinen <1292393108@qq.com>
2019-09-09 19:14:49 +0800

    main

--- example/combineFiles/main.ts
+++ example/combineFiles/main.ts
@@ -30,3 +30,30 @@ function combineFiles(pathnames: [], callback):void {
     }
   })(0, pathnames.length)
 }
+
+function main(argv: number[]) {
+  let config: any = JSON.parse(fs.reFileSync(argv[0], 'utf-8'))
+  const root: string = config.root || ''
+  const port: number = config.port || 80
+
+  http.createServer((req, res) => {
+    let urlInfo: any = parseURL(root, req.url)
+    const { pathnames, mime} = urlInfo
+
+    combineFiles(pathnames, (err, data) => {
+      if(err) {
+        res.writeHead(404)
+        res.end(err.message)
+      } else {
+        res.writeHead(200, {
+          'Content-Type': mime
+        })
+        res.end(data)
+      }
+    })
+  }).listeb(port)
+}
+
+function parseURL (root: string, url: string) {
+
+}
