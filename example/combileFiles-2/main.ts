8f0aba8c1634daf57b3ee9101100017ab32ff87c
berlinen <1292393108@qq.com>
2019-09-15 23:41:41 +0800

    update

--- example/combileFiles-2/main.ts
+++ example/combileFiles-2/main.ts
@@ -1,7 +1,9 @@
 const fs = require('fs')
 const http = require('http')
+const outputFiles = require('./core')
+const validateFiles = require('./core')
 
-function main(argv: string[]):void {
+module.exports = function main(argv: string[]):void {
   const config: any = JSON.parse(fs.readFileSync(argv[0], 'utf8'))
   let root: string = config.root || '.'
   let port: number = config.port || 80
@@ -22,40 +24,6 @@ function main(argv: string[]):void {
     })
   }).listen(port)
 }
-
-function outputFiles (pathnames:string, writer) {
-  (function next(i: number, len: number) {
-   if(i < len) {
-    var reader: any = fs.createReadStream(pathnames[i]);
-
-    reader.pipe(writer, {end: false})
-    reader.on('end', function() {
-      next(i + 1, len);
-    });
-   } else {
-    writer.end();
-   }
-  })(0, pathnames.length)
-}
-
-function validateFiles (pathnames: string, callback) {
-  (function next(i: number, len: number) {
-    if(i < len) {
-      fs.stat(pathnames[i], (err: any, stats: any) => {
-        if (err) {
-          callback(err)
-        } else if((!stats.isFile()) {
-          callback(new Error());
-        } else {
-          next(i + 1, len);
-        }
-      })
-    } else {
-      callback(null, pathnames);
-    }
-  })(0, pathnames.length)
-}
-
 
 
 export {}
