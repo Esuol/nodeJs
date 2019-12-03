7bf0bbd711b8f6dd6f6ffb46c72d37fa4519bf64
berlinen <1292393108@qq.com>
2019-12-03 14:42:10 +0800

    🚡 every middleware

--- example/generator/app.js
+++ example/generator/app.js
@@ -2,23 +2,40 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 // all middleware
 const favicon = require('serve-favicon')
+const logger = require('morgan')
+const methodOverride = require('method-override')
+const session = require('express-session')
+var errorHandler = require('errorhandler')
+
+const multer = require('multer')
+// var upload = multer({ dest: 'uploads/' })
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
-// all environments
-app.use(favicon(path.join(__dirname, '/public/favicon.ico')))
+var app = express();
 
 // view engine setup
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+// all environments
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')))
+app.use(methodOverride())
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'uwotm8'
+}))
+// 如果你需要处理一个只有文本域的表单，你应当使用 .none():
+// app.use(upload()) error
+// error handling middleware should be loaded after the loading the routes
+if (app.get('env') === 'development') {
+  app.use(errorHandler())
+}
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
