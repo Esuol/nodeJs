428f3fa67371e1567dbd2913592da88841449477
berlinen <1292393108@qq.com>
2019-12-30 15:53:54 +0800

    feature: post

--- example/node/proxy-copy.js
+++ example/node/proxy-copy.js
@@ -77,3 +77,18 @@ let proxyHttp = (options) => {
 
 module.exports = proxyHttp;
 
+
+//用于处理所有进入 3000 端口 post 的连接请求
+app.post('/',function(req,res){
+    var buffer = [];
+    //监听 data 事件 用于接收数据
+    req.on('data',function(data){
+        buffer.push(data);
+    });
+    //监听 end 事件 用于处理接收完成的数据
+    req.on('end',function(){
+    //输出接收完成的数据
+         console.log(Buffer.concat(buffer).toString('utf-8'));
+    });
+});
+
