313b7faf9d6bf5798580a00b99daa1d4346beb3d
berlinen <1292393108@qq.com>
2020-01-11 19:20:25 +0800

    feat: 子进程启动服务

---
+++ example/node/child/childOn/index.js
@@ -0,0 +1,13 @@
+const cp = requie('child_process')
+
+const child1 = cp.fork('child.js')
+const child2 = cp.fork('child.js')
+
+const server = require('net').createServer()
+
+server.listen(1337, () => {
+  child1.send('server', server)
+  child2.send('server', server)
+  // close
+  server.close()
+})
