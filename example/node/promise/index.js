a1c8d8f272ad31a6a7f6add384b39350be2f7c25
berlinen <1292393108@qq.com>
2020-01-16 11:13:28 +0800

    feature: all 实现

--- example/node/promise/index.js
+++ example/node/promise/index.js
@@ -61,3 +61,37 @@ Promise.all(promises)
     (arr) => assert.deepEqual(
       arr, ['First!', 'Second!']
 ));
+
+// promise all 简洁版实现
+
+function all(iterable) {
+  return new Promise((resolve, reject) => {
+    let index = 0;
+    for (const promise of iterable) {
+      // Capture the current value of `index`
+      const currentIndex = index;
+      promise.then(
+        (value) => {
+          if (anErrorOccurred) return;
+          result[currentIndex] = value;
+          elementCount++;
+          if (elementCount === result.length) {
+            resolve(result);
+          }
+        },
+        (err) => {
+          if (anErrorOccurred) return;
+          anErrorOccurred = true;
+          reject(err);
+        });
+      index++;
+    }
+    if (index === 0) {
+      resolve([]);
+      return;
+    }
+    let elementCount = 0;
+    let anErrorOccurred = false;
+    const result = new Array(index);
+  });
+}
